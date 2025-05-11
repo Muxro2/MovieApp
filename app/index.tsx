336bf203ebe0c876edf088fa64c639b616557213
Mo Roshid <mubynr@gmail.com>
2025-05-11 18:12:06 +0100

    pull test

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-primary text-5xl">Hello Mo</Text>
+      <Text className="text-primary text-5xl">Hello Bri</Text>
       <Link href={{pathname: "/movie/[id]",
       params: { id: "Avengers"}}}>
         Avengers
