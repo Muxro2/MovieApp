572c3a7a469f008799ca303dbdd1f5758b0f2499
Mo R <mubynr@gmail.com>
2025-05-11 15:00:14 +0100

    Initial Commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-primary text-5xl">Hello Mo</Text>
+      <Link href={{pathname: "/movie/[id]",
+      params: { id: "Avengers"}}}>
+        Avengers
+      </Link>
+    </View>
+  );
+}
